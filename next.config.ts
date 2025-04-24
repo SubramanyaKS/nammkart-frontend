fddc692e568f5a3c16634fbd8765f9abed61a029
SubramanyaKS <subramanyaks2205@gmail.com>
2025-04-24 21:55:09 +0530

    added latest packages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Wildcard to allow all hosts
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Wildcard to allow all hosts over HTTP (if needed)
+      },
+    ],
+  }
+  
 };
 
 export default nextConfig;
