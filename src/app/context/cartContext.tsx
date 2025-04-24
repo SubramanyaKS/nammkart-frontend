92568b7b7da8a03f5ecec3de60ad623423a3e1d1
SubramanyaKS <subramanyaks2205@gmail.com>
2025-04-24 21:53:31 +0530

    added cart items

---
+++ src/app/context/cartContext.tsx
@@ -0,0 +1,36 @@
+'use client'
+import React, { createContext, useReducer, useContext, ReactNode, useEffect } from 'react';
+
+
+import { cartReducer } from '../reducer/cart';
+import { CartAction, CartState } from '../utils/types';
+import { getStoredCart } from '../utils/api';
+
+const initialState: CartState = { items: [] };
+
+const CartContext = createContext<{
+  state: CartState;
+  dispatch: React.Dispatch<CartAction>;
+} | null>(null);
+
+export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, getStoredCart());
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state));
+  }, [state]);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
